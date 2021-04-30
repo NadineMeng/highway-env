a612d9c779a034e08ab3bb5240057da5ca701cf1
Danial Kamran <danial.kamran@kit.edu>
2021-04-30 11:10:07 +0200

    make environment ready for merging tests

--- highway_env/envs/merge_env.py
+++ highway_env/envs/merge_env.py
@@ -19,8 +19,8 @@ class MergeEnv(AbstractEnv):
     It is rewarded for maintaining a high speed and avoiding collisions, but also making room for merging
     vehicles.
     """
-
-    COLLISION_REWARD: float = -1
+    SUCCESS_REWARD: float = 1.
+    COLLISION_REWARD: float = -1.
     RIGHT_LANE_REWARD: float = 0.1
     HIGH_SPEED_REWARD: float = 0.2
     MERGING_SPEED_REWARD: float = -0.5
@@ -35,31 +35,42 @@ class MergeEnv(AbstractEnv):
         :param action: the action performed
         :return: the reward of the state-action transition
         """
-        action_reward = {0: self.LANE_CHANGE_REWARD,
-                         1: 0,
-                         2: self.LANE_CHANGE_REWARD,
-                         3: 0,
-                         4: 0}
-        reward = self.COLLISION_REWARD * self.vehicle.crashed \
-                 + self.RIGHT_LANE_REWARD * self.vehicle.lane_index[2] / 1 \
-                 + self.HIGH_SPEED_REWARD * self.vehicle.speed_index / (self.vehicle.SPEED_COUNT - 1)
-
-        # Altruistic penalty
-        for vehicle in self.road.vehicles:
-            if vehicle.lane_index == ("b", "c", 2) and isinstance(vehicle, ControlledVehicle):
-                reward += self.MERGING_SPEED_REWARD * \
-                          (vehicle.target_speed - vehicle.speed) / vehicle.target_speed
-
-        return utils.lmap(action_reward[action] + reward,
-                          [self.COLLISION_REWARD + self.MERGING_SPEED_REWARD,
-                            self.HIGH_SPEED_REWARD + self.RIGHT_LANE_REWARD],
-                          [0, 1])
+        # action_reward = {0: self.LANE_CHANGE_REWARD,
+        #                  1: 0,
+        #                  2: self.LANE_CHANGE_REWARD,
+        #                  3: 0,
+        #                  4: 0}
+        # reward = self.COLLISION_REWARD * self.vehicle.crashed \
+        #          + self.RIGHT_LANE_REWARD * self.vehicle.lane_index[2] / 1 \
+        #          + self.HIGH_SPEED_REWARD * self.vehicle.speed_index / (self.vehicle.SPEED_COUNT - 1)
+        #
+        # # Altruistic penalty
+        # for vehicle in self.road.vehicles:
+        #     if vehicle.lane_index == ("b", "c", 2) and isinstance(vehicle, ControlledVehicle):
+        #         reward += self.MERGING_SPEED_REWARD * \
+        #                   (vehicle.target_speed - vehicle.speed) / vehicle.target_speed
+        #
+        # return utils.lmap(action_reward[action] + reward,
+        #                   [self.COLLISION_REWARD + self.MERGING_SPEED_REWARD,
+        #                     self.HIGH_SPEED_REWARD + self.RIGHT_LANE_REWARD],
+        #                   [0, 1])
+        r = 0.
+        if self.vehicle.crashed:
+            r = self.COLLISION_REWARD
+        if self.vehicle.position[0] > 320:
+            r = self.SUCCESS_REWARD
+        #print("Reward: {}".format(r))
+        return r
 
     def _is_terminal(self) -> bool:
         """The episode is over when a collision occurs or when the access ramp has been passed."""
-        return self.vehicle.crashed or self.vehicle.position[0] > 320
+        terminal = self.vehicle.crashed or self.vehicle.position[0] > 320
+        #if terminal:
+        #    print("Terminal......................")
+        return terminal
 
     def _reset(self) -> None:
+        #TODO: add lower speed also
         #high_speed
         vehicles_density=np.random.uniform(0.6,1.5)
         avg_speed = 30.
@@ -84,17 +95,22 @@ class MergeEnv(AbstractEnv):
                 "features_range": {
                     "x": [-100, 100],
                     "y": [-100, 100],
-                    "vx": [-20, 20],
-                    "vy": [-20, 20]
+                    "vx": [-40, 40],
+                    "vy": [-40, 40]
                 },
                 "absolute": False,
-                "normalize":False,
+                "normalize":True,
                 "order": "sorted"
+            },
+            "action": {
+                "type": "DiscreteMetaAction",
+                "longitudinal": True,
+                "lateral": False
             },
             "vehicles_count":20,
             "policy_frequency": 2,
             "duration": 40,
-            'real_time_rendering': True,
+            'real_time_rendering': False,
         })
         return config
 
@@ -122,13 +138,13 @@ class MergeEnv(AbstractEnv):
         ljk = StraightLane([0, 6.5 + 4 + 4], [ends[0], 6.5 + 4 + 4], line_types=[c, c], forbidden=True)
         lkb = SineLane(ljk.position(ends[0], -amplitude), ljk.position(sum(ends[:2]), -amplitude),
                        amplitude, 2 * np.pi / (2*ends[1]), np.pi / 2, line_types=[c, c], forbidden=True)
-        lbc = StraightLane(lkb.position(ends[1], 0), lkb.position(ends[1], 0) + [ends[2], 0],
+        lbc = StraightLane(lkb.position(ends[1], -StraightLane.DEFAULT_WIDTH), lkb.position(ends[1], -StraightLane.DEFAULT_WIDTH) + [ends[2], 0],
                            line_types=[n, c], forbidden=True)
         net.add_lane("j", "k", ljk)
         net.add_lane("k", "b", lkb)
         net.add_lane("b", "c", lbc)
         road = Road(network=net, np_random=self.np_random, record_history=self.config["show_trajectories"])
-        road.objects.append(Obstacle(road, lbc.position(ends[2], 0)))
+        #road.objects.append(Obstacle(road, lbc.position(ends[2], 0)))
         self.road = road
 
     def _make_vehicles(self) -> None:
@@ -141,12 +157,12 @@ class MergeEnv(AbstractEnv):
 
 
         other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
-        print(self.config["other_vehicles_type"])
+        #print(self.config["other_vehicles_type"])
         # road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(90, 0), speed=29))
         # road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 1)).position(70, 0), speed=31))
         # road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(5, 0), speed=31.5))
         ego_vehicle = self.action_type.vehicle_class(road,
-                                                     road.network.get_lane(("j", "k", 0)).position(140, 0), speed=30)
+                                                     road.network.get_lane(("j", "k", 0)).position(140, 0), speed=self.config["avg_speed"])
         for _ in range(self.config["vehicles_count"]):
 
             lanes = np.arange(2)
@@ -160,13 +176,13 @@ class MergeEnv(AbstractEnv):
                                                   lane_id=lane_id,
                                                   speed=speed,
                                                   spacing=1 / self.config["vehicles_density"],
-                                                  ).plan_route_to("d")
+                                                  )
 
 
             #
             #new_vehicle = other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"],
             #                                                speed=speed)
-            new_vehicle.enable_lane_change = np.random.random()<0.5
+            new_vehicle.enable_lane_change = False#np.random.random()<0.5
             self.road.vehicles.append(new_vehicle)
 
         #IMPORTANT: Ego vehicle should be added after others!
